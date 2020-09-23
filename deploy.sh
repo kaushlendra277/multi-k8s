3ba299ad5bd284a6dca6679965ceaf30aeb5c9f3
kaushlendra <kaushlendra.chauhan@xoriant.com>
2020-09-23 17:01:11 +0530

    comment SHA tags

--- deploy.sh
+++ deploy.sh
@@ -1,21 +1,21 @@
 # 7. Build all our images, tag each one, push each other to docker hub - Start
 # 7.1 Building images, and tagging - start
-docker build -t kaushlendra277/multi-client:latest -t kaushlendra277/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t kaushlendra277/multi-server:latest -t kaushlendra277/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t kaushlendra277/multi-worker:latest -t kaushlendra277/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker build -t kaushlendra277/multi-client:latest  -f ./client/Dockerfile ./client
+docker build -t kaushlendra277/multi-server:latest  -f ./server/Dockerfile ./server
+docker build -t kaushlendra277/multi-worker:latest  -f ./worker/Dockerfile ./worker
 # 7.1 Building images, and tagging - - end
 
 # 7.2 push each other to docker hub - start
 # To push docker images create kaushlendra277/multi-client in docker hub first
 echo "pushing multi-client"
 docker push kaushlendra277/multi-client:latest
-docker push kaushlendra277/multi-client:$SHA
-echo "pushing multi-server"
+# docker push kaushlendra277/multi-client:$SHA
+echo "pushing multi-server"SHA
 docker push kaushlendra277/multi-server:latest
-docker push kaushlendra277/multi-server:$SHA
+# docker push kaushlendra277/multi-server:$SHA
 echo "pushing multi-worker"
 docker push kaushlendra277/multi-worker:latest
-docker push kaushlendra277/multi-worker:$SHA
+# docker push kaushlendra277/multi-worker:$SHA
 # 7.2 push each other to docker hub - end
 # 7. Build all our images, tag each one, push each other to docker hub - end
 
@@ -24,7 +24,7 @@ kubectl apply -f k8s
 # 8. Apply all configs in the 'k8s' folder - end
 
 # 9. Imperatively set latest images on each deployment - Start
-kubectl set image deployments/client-deployment client=kaushlendra277/multi-client:$SHA
-kubectl set image deployments/server-deployment server=kaushlendra277/multi-server:$SHA
-kubectl set image deployments/worker-deployment worker=kaushlendra277/multi-worker:$SHA
+# kubectl set image deployments/client-deployment client=kaushlendra277/multi-client:$SHA
+# kubectl set image deployments/server-deployment server=kaushlendra277/multi-server:$SHA
+# kubectl set image deployments/worker-deployment worker=kaushlendra277/multi-worker:$SHA
 # 9. Imperatively set latest images on each deployment - end
